272473410957ce3cc6d36a815d79c33dde96819d
ZhaoQY <zhao_qiyuan@126.com>
2024-11-14 18:10:39 +0800

    PGD算法收敛了，但还在摸索lam的参数值“

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -72,80 +72,67 @@ class ImageProcessor:
 
         self.noisy_images.append(Image.fromarray(noisy_image))
 
-    def PGM_denoising(self, index_image,lam,t):
+    def PGD_denoising(self, index_image,lam,t):
         # 近段梯度下降去噪点函数
-        image_array = np.array(self.noisy_images[index_image])
+        image_array = np.array(self.noisy_images[index_image],dtype=np.float64) 
         # 这里正则项选择L1范数，保真项为||y-x||^2
-        k = 0
-        image_array_k_old = image_array
-        image_array_k = image_array
-        while k < 1e5:
+        image_array_k_old = image_array.copy()
+        image_array_k = image_array.copy()
+        for i in range(1, 10000):
+            # 梯度下降
             image_array_k_temp = image_array_k_old - t * (image_array_k_old - image_array)
             # 软阈值函数
-            for i, row in enumerate(image_array_k_temp):
-                for j, pixel in enumerate(row):
-                    for k, channel in enumerate(pixel):
+            for x, row in enumerate(image_array_k_temp):
+                for y, pixel in enumerate(row):
+                    for z, channel in enumerate(pixel):
                         if channel > lam * t:
-                            image_array_k[i][j][k] = channel - lam * t
+                            image_array_k[x][y][z] = channel - lam * t
                         elif abs(channel) < lam * t:
-                            image_array_k[i][j][k] = 0
+                            image_array_k[x][y][z] = 0
                         elif channel < -lam * t:
-                            image_array_k[i][j][k] = channel + lam * t                  
-#                    if (pixel > lam * t).all():
-#                        image_array_k[i][j] = pixel - lam * t
-#                    elif (pixel < -lam * t).all():
-#                        image_array_k[i][j] = pixel + lam * t
-#                    else:
-#                        image_array_k[i][j] = 0
-            if np.linalg.norm(image_array_k - image_array_k_old) < 1e-4:
+                            image_array_k[x][y][z] = channel + lam * t    
+            norm_diff = np.linalg.norm(image_array_k - image_array_k_old)                           
+            if norm_diff < 1e-4:
                 break
             else:
                 image_array_k_old = image_array_k.copy()
-                k += 1
-        print(f'迭代次数为{k}')
+        print(f'迭代次数为{i}')
         return Image.fromarray(image_array_k.astype(np.uint8))
                         
     def ADMM_denoising(self, index_image, lam, t1, t2, alpha_k):
         # ADMM去噪点函数
-        image_array = np.array(self.noisy_images[index_image])
+        image_array = np.array(self.noisy_images[index_image],dtype=np.float64)
         # 这里正则项选择L1范数，保真项为||y-x||^2
-        k = 0
         image_array_k_old = np.ones_like(image_array)
         u_k_old = np.zeros_like(image_array)
         s_k_old = np.zeros_like(image_array)
         u_k = np.zeros_like(image_array)
-        while k < 1e6:
+        for i in range(1, 1000):
             # 更新image_array
             image_array_k = (1 + 2*t2) * (image_array + t2 * u_k_old - s_k_old) 
 
             # 更新u_k
             u_k_temp = u_k_old + t1 * t2 * (image_array_k - u_k_old + (1/t2) * s_k_old)
-            for i, row in enumerate(u_k_temp):
-                for j, pixel in enumerate(row):
-                    for k, channel in enumerate(pixel):
+            for x, row in enumerate(u_k_temp):
+                for y, pixel in enumerate(row):
+                    for z, channel in enumerate(pixel):
                         if channel > lam * t1:
-                            u_k[i][j][k] = channel - lam * t1
+                            u_k[x][y][z] = channel - lam * t1
                         elif abs(channel) < lam * t1:
-                            u_k[i][j][k] = 0
+                            u_k[x][y][z] = 0
                         elif channel < -lam * t1:
-                            u_k[i][j][k] = channel + lam * t1
-#                   if (pixel > lam * t1).all():
-#                        u_k[i][j] = pixel - lam * t1
-#                    elif (pixel < -lam * t1).all():
-#                        u_k[i][j] = pixel + lam * t1
-#                    else:
-#                        u_k[i][j] = 0
+                            u_k[x][y][z] = channel + lam * t1
 
             # 更新s_k
             s_k = s_k_old + alpha_k * (image_array_k - u_k)
-            if np.linalg.norm(image_array_k - image_array_k_old) < 1e-4:
+            if np.linalg.norm(image_array_k - image_array_k_old) < 1e-3:
                 break    
             else:
                 image_array_k_old = image_array_k.copy()
                 u_k_old = u_k.copy()
                 s_k_old = s_k.copy()
                 k += 1
-        print(f'迭代次数为{k}')
+        print(f'迭代次数为{i}')
         return Image.fromarray(image_array_k.astype(np.uint8))
 
 
